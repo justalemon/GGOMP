7f7a55e57386a3a7a3f5ef3b586aa3f36c9761c9
Hannele Ruiz <justlemoncl@gmail.com>
2018-11-05 11:31:21 -0300

    Added basic script with an event for when the player joins

---
+++ GGO.Server/Script.cs
@@ -0,0 +1,26 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Threading.Tasks;
+
+namespace GGO.Server
+{
+    public class ScriptServer : BaseScript
+    {
+        public ScriptServer()
+        {
+            Tick += OnTick;
+            EventHandlers.Add("playerConnecting", new Action<string, CallbackDelegate>(OnPlayerConnecting));
+        }
+
+        private async Task OnTick()
+        {
+            await Delay(100);
+        }
+
+        private void OnPlayerConnecting(string Name, CallbackDelegate SetReason)
+        {
+
+        }
+    }
+}
