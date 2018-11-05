b4c2aa0e4a7adecec703a58548933cbebe8dbf58
Hannele Ruiz <justlemoncl@gmail.com>
2018-11-05 13:21:49 -0300

    Added more events on the Server side

--- GGO.Server/Script.cs
+++ GGO.Server/Script.cs
@@ -1,6 +1,7 @@
 ﻿using CitizenFX.Core;
 using CitizenFX.Core.Native;
 using System;
+using System.Dynamic;
 using System.Threading.Tasks;
 
 namespace GGO.Server
@@ -10,7 +11,10 @@ namespace GGO.Server
         public ScriptServer()
         {
             Tick += OnTick;
-            EventHandlers.Add("playerConnecting", new Action<string, CallbackDelegate>(OnPlayerConnecting));
+            EventHandlers.Add("playerConnecting", new Action<string, CallbackDelegate, ExpandoObject>(OnPlayerConnecting));
+            EventHandlers.Add("playerDropped", new Action<string, Player>(OnPlayerDropped));
+            EventHandlers.Add("onPlayerDied", new Action<Player, string, Vector3>(OnPlayerDied));
+            EventHandlers.Add("onPlayerKilled", new Action<Player, Player, string, Vector3>(OnPlayerKilled));
         }
 
         private async Task OnTick()
@@ -18,7 +22,27 @@ namespace GGO.Server
             await Delay(100);
         }
 
-        private void OnPlayerConnecting(string Name, CallbackDelegate SetReason)
+        private void OnPlayerConnecting(string Name, CallbackDelegate SetReason, ExpandoObject TempPlayer)
+        {
+
+        }
+
+        private void OnPlayerDropped(string Reason, Player Source)
+        {
+
+        }
+
+        private void OnPlayerDied(Player DeadPlayer, string Reason, Vector3 Position)
+        {
+
+        }
+
+        private void OnPlayerKilled(Player DeadPlayer, Player Killer, string Reason, Vector3 Position)
+        {
+
+        }
+
+        private void OnPlayerSpawned()
         {
 
         }
