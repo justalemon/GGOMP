fec9b55734432417be7e4bbfadc6fbf6d3d77c36
Hannele Ruiz <justlemoncl@gmail.com>
2018-11-05 19:43:41 -0300

    Added option to disable the NPC peds and traffic

--- GGO.Client/Script.cs
+++ GGO.Client/Script.cs
@@ -1,4 +1,5 @@
 using CitizenFX.Core;
+using CitizenFX.Core.Native;
 using System;
 using System.Threading.Tasks;
 
@@ -14,7 +15,21 @@ namespace GGO.Client
 
         private async Task OnTick()
         {
-            await Delay(100);
+            if (!Convert.ToBoolean(API.GetConvar("ggo_npcs", "false")))
+            {
+                Vector3 PlayerPosition = LocalPlayer.Character.Position;
+
+                API.SetVehicleDensityMultiplierThisFrame(0);
+                API.SetPedDensityMultiplierThisFrame(0);
+                API.SetRandomVehicleDensityMultiplierThisFrame(0);
+                API.SetParkedVehicleDensityMultiplierThisFrame(0);
+                API.SetScenarioPedDensityMultiplierThisFrame(0, 0);
+                API.RemoveVehiclesFromGeneratorsInArea(PlayerPosition.X - 500, PlayerPosition.Y - 500, PlayerPosition.Z - 500, PlayerPosition.X + 500, PlayerPosition.Y + 500, PlayerPosition.Z + 500, 0);
+                API.SetGarbageTrucks(false);
+                API.SetRandomBoats(false);
+            }
+
+            await Delay(1);
         }
 
         private void OnClientMapStart()
