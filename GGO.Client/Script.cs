aa170ef19f223a52ce74ea8976f3b4117b36842c
Hannele Ruiz <justlemoncl@gmail.com>
2018-11-06 13:11:55 -0300

    Added HUB system with custom spawns

--- GGO.Client/Script.cs
+++ GGO.Client/Script.cs
@@ -1,17 +1,24 @@
 using CitizenFX.Core;
 using CitizenFX.Core.Native;
+using GGO.Shared;
 using System;
+using System.Dynamic;
 using System.Threading.Tasks;
 
 namespace GGO.Client
 {
     public class ScriptClient : BaseScript
     {
+        /// <summary>
+        /// Generator of random values.
+        /// </summary>
+        public static Random Generator = new Random();
+
         public ScriptClient()
         {
             Tick += OnTick;
-            EventHandlers.Add("onClientMapStart", new Action(OnClientMapStart));
-            EventHandlers.Add("playerSpawned", new Action<Player, Vector3>(OnPlayerSpawned));
+            EventHandlers.Add("onClientGameTypeStart", new Action(OnClientGameTypeStart));
+            EventHandlers.Add("playerSpawned", new Action<ExpandoObject, Vector3>(OnPlayerSpawn));
         }
 
         private async Task OnTick()
@@ -33,10 +40,47 @@ namespace GGO.Client
             await Delay(1);
         }
 
-        private void OnClientMapStart()
+        private void OnClientGameTypeStart()
         {
-            Exports["spawnmanager"].setAutoSpawn(true);
+            Exports["spawnmanager"].setAutoSpawnCallback(new Action(SpawnPlayerCallback));
             Exports["spawnmanager"].forceRespawn();
+
+            API.RequestIpl("prologue06_int");
+
+            foreach (string IPL in Data.NorthYankton)
+            {
+                // API.RequestIpl(IPL);
+            }
+        }
+
+        private void OnPlayerSpawn(ExpandoObject Spawned, Vector3 Where)
+        {
+            if (LocalPlayer.Character.Model == new Model("mp_m_freemode_01"))
+            {
+                API.SetPedComponentVariation(LocalPlayer.Character.GetHashCode(), 0, 0, 0, 2);  // Face
+                API.SetPedComponentVariation(LocalPlayer.Character.GetHashCode(), 2, 11, 4, 2); // Hair
+                API.SetPedComponentVariation(LocalPlayer.Character.GetHashCode(), 4, 1, 5, 2);  // Pants
+                API.SetPedComponentVariation(LocalPlayer.Character.GetHashCode(), 6, 1, 0, 2);  // Shoes
+                API.SetPedComponentVariation(LocalPlayer.Character.GetHashCode(), 11, 7, 2, 2); // Jacket
+            }
+            
+            if (API.IsPlayerSwitchInProgress())
+            {
+                API.ShowLoadingPrompt(1);
+            }
+
+            API.SwitchInPlayer(LocalPlayer.Character.GetHashCode());
+        }
+
+        private void SpawnPlayerCallback()
+        {
+            if (!API.IsPlayerSwitchInProgress())
+            {
+                API.SwitchOutPlayer(LocalPlayer.Character.GetHashCode(), 32, 1);
+            }
+
+            Location SpawnLocation = Data.HubSpawns[Generator.Next(Data.HubSpawns.Length)];
+            Exports["spawnmanager"].spawnPlayer(new { x = SpawnLocation.X, y = SpawnLocation.Y, z = SpawnLocation.Z, heading = SpawnLocation.R, model = "mp_m_freemode_01" });
         }
     }
 }
