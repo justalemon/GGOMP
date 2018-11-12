3f61cf8bc8df6263ba2736863dcc890fc7874eb1
Hannele Ruiz <justlemoncl@gmail.com>
2018-11-12 18:52:36 -0300

    Added loading of NativeUI on the Client side

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,8 @@ resource_type "gametype" { name = "GGO" }
 
 client_scripts {
 	"GGO.Client.net.dll",
-	"GGO.Shared.net.dll"
+	"GGO.Shared.net.dll",
+	"NativeUI.net.dll"
 }
 server_scripts {
 	"GGO.Server.net.dll",
