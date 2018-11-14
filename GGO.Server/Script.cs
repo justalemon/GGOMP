6baef554acbf1e86dc55b439d1989e09c8a608dc
Hannele Ruiz <justlemoncl@gmail.com>
2018-11-14 18:24:55 -0300

    Avoid the bloated code by using a separate function

--- GGO.Server/Script.cs
+++ GGO.Server/Script.cs
@@ -28,65 +28,53 @@ namespace GGO.Server
             PlayerList Players = new PlayerList();
             int PlayerCount = API.GetNumPlayerIndices();
             int MinimumPlayers = API.GetConvarInt("ggo_minplayers", 1);
-
             
             // If the player count is higher or equal to the minimum and the current status is not "enough players"
             if (PlayerCount >= MinimumPlayers && Status != GameStatus.EnoughPlayers)
             {
                 // Store the log message
                 string Message = string.Format(Resources.MatchEnoughPlayers, API.GetConvarInt("ggo_gamestart", 1));
-
                 // Write a note into the server console
                 Debug.WriteLine(Message.Replace("~n~", " "));
-
                 // Store the status on a variable
                 Status = GameStatus.EnoughPlayers;
-
                 // And notify all of the players
-                foreach (Player NotifyTo in new PlayerList())
-                {
-                    TriggerClientEvent(NotifyTo, "onMatchStart", false, Message);
-                }
+                NotifyPlayers(Message, false);
             }
             // If the player count is lower than the required one and the current status is "enough players"
             else if (PlayerCount < MinimumPlayers)
             {
                 // Store the log message
                 string Message = string.Format(Resources.MatchNotEnoughPlayers, MinimumPlayers - PlayerCount, API.GetConvarInt("ggo_gamestart", 1));
-
                 // Write a note into the server console
                 Debug.WriteLine(Message.Replace("~n~", " "));
-
                 // Store the status on a variable
                 Status = GameStatus.WaitingForPlayers;
-
                 // And notify all of the players
-                foreach (Player NotifyTo in new PlayerList())
-                {
-                    TriggerClientEvent(NotifyTo, "onMatchStart", false, Message);
-                }
+                NotifyPlayers(Message, false);
             }
             // If none of the later match
             else if (Status == GameStatus.EnoughPlayers)
             {
-                // Store the log message
-                string Message = Resources.MatchStartingUp;
-
                 // Write a note into the server console
-                Debug.WriteLine(Message.Replace("~n~", " "));
-
+                Debug.WriteLine(Resources.MatchStartingUp.Replace("~n~", " "));
                 // Store the status on a variable
                 Status = GameStatus.WaitingForPlayers;
-
                 // And notify all of the players
-                foreach (Player NotifyTo in new PlayerList())
-                {
-                    TriggerClientEvent(NotifyTo, "onMatchStart", false, Message);
-                }
+                NotifyPlayers(Resources.MatchStartingUp);
             }
 
             // Try again in the specified number of minutes (default: 1)
             await Delay(API.GetConvarInt("ggo_gamestart", 1) * 60 * 1000);
+        }
+
+        public void NotifyPlayers(string Message, bool Started = false)
+        {
+            // And notify all of the players
+            foreach (Player NotifyTo in new PlayerList())
+            {
+                TriggerClientEvent(NotifyTo, "onMatchStart", false, Message);
+            }
         }
     }
 }
