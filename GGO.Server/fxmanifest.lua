8a64cf34dced54f7c2aba5470c0e42d97da4c965
Hannele Ruiz <justlemoncl@gmail.com>
2020-05-25 18:52:42 -0400

    Streamed files and Metadata are now copied to output folder

---
+++ GGO.Server/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "bodacious"
+game "gta5"
+resource_type "gametype" { name = "GGO by Lemon" }
+
+client_script "GGO.Client.net.dll"
+client_script "GGO.Shared.net.dll"
+client_script "NativeUI.net.dll"
+
+server_script "GGO.Server.net.dll"
+server_script "GGO.Shared.net.dll"
